934c999436342793f8a9214f373b52f23db494da
furmangg_cp <furmangg@users.noreply.github.com>
2008-03-13 02:02:28 +0000

    installer changes for release 0.5.1 no functionality changes, just installer fixes

--- SetSecurity/SetSecurity.cs
+++ SetSecurity/SetSecurity.cs
@@ -28,121 +28,165 @@ namespace CustomActions
 
         public override void Install(System.Collections.IDictionary stateSaver)
         {
-            // Call the base implementation.
-            base.Install(stateSaver);
-
-            string allUsersString = this.Context.Parameters["allUsers"];
-            string solutionCodeGroupName = this.Context.Parameters["solutionCodeGroupName"];
-            string solutionCodeGroupDescription = this.Context.Parameters["solutionCodeGroupDescription"];
-            string targetDir = this.Context.Parameters["targetDir"];
-            string assemblyName = this.Context.Parameters["assemblyName"];
-            string assemblyCodeGroupName = this.Context.Parameters["assemblyCodeGroupName"];
-            string assemblyCodeGroupDescription = this.Context.Parameters["assemblyCodeGroupDescription"];
-
-            // Note that a code group with solutionCodeGroupName name is created in the 
-            // Install method and removed in the Rollback and Uninstall methods.
-            // The solutionCodeGroupName must be a unique name to ensure that the 
-            // correct code group is removed during Rollback and Uninstall.
-
-            if (String.IsNullOrEmpty(solutionCodeGroupName))
-                throw new InstallException("Cannot set the security policy. The specified solution code group name is not valid.");
-            if (String.IsNullOrEmpty(solutionCodeGroupDescription))
-                throw new InstallException("Cannot set the security policy. The specified solution code group description is not valid.");
-            if (String.IsNullOrEmpty(targetDir))
-                throw new InstallException("Cannot set the security policy. The specified target directory is not valid.");
-            if (String.IsNullOrEmpty(assemblyName))
-                throw new InstallException("Cannot set the security policy. The specified assembly name is not valid.");
-            if (String.IsNullOrEmpty(assemblyCodeGroupName))
-                throw new InstallException("Cannot set the security policy. The specified assembly code group name is not valid.");
-            if (String.IsNullOrEmpty(assemblyCodeGroupDescription))
-                throw new InstallException("Cannot set the security policy. The specified assembly code group description is not valid.");
-            if (stateSaver == null)
-                throw new ArgumentNullException("stateSaver");
-
             try
             {
-                bool allUsers = String.Equals(allUsersString, "1");
-                string assemblyPath = Path.Combine(targetDir, assemblyName);
 
-                // Note that Install method may be invoked during Repair mode and the code group 
-                // may already exist.
-                // To prevent adding of another code group, remove the code group if it exists.
+                // Call the base implementation.
+                base.Install(stateSaver);
+
+                string allUsersString = this.Context.Parameters["allUsers"];
+                string solutionCodeGroupName = this.Context.Parameters["solutionCodeGroupName"];
+                string solutionCodeGroupDescription = this.Context.Parameters["solutionCodeGroupDescription"];
+                string targetDir = this.Context.Parameters["targetDir"];
+                string assemblyName = this.Context.Parameters["assemblyName"];
+                string assemblyCodeGroupName = this.Context.Parameters["assemblyCodeGroupName"];
+                string assemblyCodeGroupDescription = this.Context.Parameters["assemblyCodeGroupDescription"];
+
+                // Note that a code group with solutionCodeGroupName name is created in the 
+                // Install method and removed in the Rollback and Uninstall methods.
+                // The solutionCodeGroupName must be a unique name to ensure that the 
+                // correct code group is removed during Rollback and Uninstall.
+
+                if (String.IsNullOrEmpty(solutionCodeGroupName))
+                    throw new InstallException("Cannot set the security policy. The specified solution code group name is not valid.");
+                if (String.IsNullOrEmpty(solutionCodeGroupDescription))
+                    throw new InstallException("Cannot set the security policy. The specified solution code group description is not valid.");
+                if (String.IsNullOrEmpty(targetDir))
+                    throw new InstallException("Cannot set the security policy. The specified target directory is not valid.");
+                if (String.IsNullOrEmpty(assemblyName))
+                    throw new InstallException("Cannot set the security policy. The specified assembly name is not valid.");
+                if (String.IsNullOrEmpty(assemblyCodeGroupName))
+                    throw new InstallException("Cannot set the security policy. The specified assembly code group name is not valid.");
+                if (String.IsNullOrEmpty(assemblyCodeGroupDescription))
+                    throw new InstallException("Cannot set the security policy. The specified assembly code group description is not valid.");
+                if (stateSaver == null)
+                    throw new ArgumentNullException("stateSaver");
+
+                bool allUsers = String.Equals(allUsersString, "1");
+
                 try
                 {
-                    // The solutionCodeGroupName must be a unique name; otherwise, the method might delete wrong code group.
-                    CaspolSecurityPolicyCreator.RemoveSecurityPolicy(allUsers, solutionCodeGroupName);
+                    string assemblyPath = Path.Combine(targetDir, assemblyName);
+
+                    // Note that Install method may be invoked during Repair mode and the code group 
+                    // may already exist.
+                    // To prevent adding of another code group, remove the code group if it exists.
+                    try
+                    {
+                        // The solutionCodeGroupName must be a unique name; otherwise, the method might delete wrong code group.
+                        CaspolSecurityPolicyCreator.RemoveSecurityPolicy(allUsers, solutionCodeGroupName);
+                    }
+                    catch { }
+
+                    CaspolSecurityPolicyCreator.AddSecurityPolicy(
+                        allUsers,
+                        solutionCodeGroupName,
+                        solutionCodeGroupDescription,
+                        assemblyPath,
+                        assemblyCodeGroupName,
+                        assemblyCodeGroupDescription);
+                    stateSaver.Add("allUsers", allUsers);
+
+
                 }
-                catch {}
+                catch (Exception ex)
+                {
+                    throw new InstallException("Cannot set the security policy.", ex);
+                }
 
-                CaspolSecurityPolicyCreator.AddSecurityPolicy(
-                    allUsers,
-                    solutionCodeGroupName,
-                    solutionCodeGroupDescription,
-                    assemblyPath,
-                    assemblyCodeGroupName,
-                    assemblyCodeGroupDescription);
-                stateSaver.Add("allUsers", allUsers);
 
+                //added for OLAP PivotTable Extensions
+                ManageUserSettings.Install(allUsers);
             }
             catch (Exception ex)
             {
-                throw new InstallException("Cannot set the security policy.", ex);
+                //doesn't appear to report this exception to the user, so we have to do it ourselves
+                System.Windows.Forms.MessageBox.Show("Problem in SetSecurity.Install: " + ex.Message + "\r\n" + ex.StackTrace);
+                throw;
             }
         }
 
         public override void Rollback(System.Collections.IDictionary savedState)
         {
-            // Call the base implementation.
-            base.Rollback(savedState);
-
-            // Check whether the "allUsers" property is saved.
-            // If it is not set, the Install method did not set the security policy.
-            if ((savedState == null) || (savedState["allUsers"] == null))
-                return;
-
-            // The solutionCodeGroupName must be a unique name; otherwise, the method might delete wrong code group.
-            string solutionCodeGroupName = this.Context.Parameters["solutionCodeGroupName"];
-            if (String.IsNullOrEmpty(solutionCodeGroupName))
-                throw new InstallException("Cannot remove the security policy. The specified solution code group name is not valid.");
-
             try
             {
-                bool allUsers = (bool) savedState["allUsers"];
-                CaspolSecurityPolicyCreator.RemoveSecurityPolicy(allUsers, solutionCodeGroupName);
+                // Call the base implementation.
+                base.Rollback(savedState);
+
+                // Check whether the "allUsers" property is saved.
+                // If it is not set, the Install method did not set the security policy.
+                if ((savedState == null) || (savedState["allUsers"] == null))
+                    return;
+
+                // The solutionCodeGroupName must be a unique name; otherwise, the method might delete wrong code group.
+                string solutionCodeGroupName = this.Context.Parameters["solutionCodeGroupName"];
+                if (String.IsNullOrEmpty(solutionCodeGroupName))
+                    throw new InstallException("Cannot remove the security policy. The specified solution code group name is not valid.");
+
+                bool allUsers = (bool)savedState["allUsers"];
+
+                try
+                {
+                    CaspolSecurityPolicyCreator.RemoveSecurityPolicy(allUsers, solutionCodeGroupName);
+                }
+                catch (Exception ex)
+                {
+                    throw new InstallException("Cannot remove the security policy.", ex);
+                }
+
+                //added for OLAP PivotTable Extensions
+                ManageUserSettings.Uninstall(allUsers);
             }
             catch (Exception ex)
             {
-                throw new InstallException("Cannot remove the security policy.", ex);
+                //doesn't appear to report this exception to the user, so we have to do it ourselves
+                System.Windows.Forms.MessageBox.Show("Problem in SetSecurity.Rollback: " + ex.Message + "\r\n" + ex.StackTrace);
+                throw;
             }
         }
 
 
         public override void Uninstall(System.Collections.IDictionary savedState)
         {
-            // Call the base implementation.
-            base.Uninstall(savedState);
-
-            // Check whether the "allUsers" property is saved.
-            // If it is not set, the Install method did not set the security policy.
-            if ((savedState == null) || (savedState["allUsers"] == null))
-                return;
-
-            // The solutionCodeGroupName must be a unique name; otherwise, the method might delete wrong code group.
-            string solutionCodeGroupName = this.Context.Parameters["solutionCodeGroupName"];
-            if (String.IsNullOrEmpty(solutionCodeGroupName))
-                throw new InstallException("Cannot remove the security policy. The specified solution code group name is not valid.");
-
             try
             {
+
+                // Call the base implementation.
+                base.Uninstall(savedState);
+
+                // Check whether the "allUsers" property is saved.
+                // If it is not set, the Install method did not set the security policy.
+                if ((savedState == null) || (savedState["allUsers"] == null))
+                    return;
+
+                // The solutionCodeGroupName must be a unique name; otherwise, the method might delete wrong code group.
+                string solutionCodeGroupName = this.Context.Parameters["solutionCodeGroupName"];
+                if (String.IsNullOrEmpty(solutionCodeGroupName))
+                    throw new InstallException("Cannot remove the security policy. The specified solution code group name is not valid.");
+
                 bool allUsers = (bool)savedState["allUsers"];
-                CaspolSecurityPolicyCreator.RemoveSecurityPolicy(allUsers, solutionCodeGroupName);
+
+                try
+                {
+                    CaspolSecurityPolicyCreator.RemoveSecurityPolicy(allUsers, solutionCodeGroupName);
+                }
+                catch (Exception ex)
+                {
+                    // Note that throwing an exception might stop the uninstall process.
+                    // To inform the user and stop the uninstall process, throw an exception.
+                    // To continue the uninstall, do not throw the exception.
+                    throw new InstallException("Cannot remove the security policy.", ex);
+                }
+
+
+                //added for OLAP PivotTable Extensions
+                ManageUserSettings.Uninstall(allUsers);
             }
             catch (Exception ex)
             {
-                // Note that throwing an exception might stop the uninstall process.
-                // To inform the user and stop the uninstall process, throw an exception.
-                // To continue the uninstall, do not throw the exception.
-                throw new InstallException("Cannot remove the security policy.", ex);
+                //doesn't appear to report this exception to the user, so we have to do it ourselves
+                System.Windows.Forms.MessageBox.Show("Problem in SetSecurity.Uninstall: " + ex.Message + "\r\n" + ex.StackTrace);
+                throw;
             }
         }
     }
